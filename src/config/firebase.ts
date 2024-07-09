a5850b0e6bcb4e5a2424ecd857ac98ccbbcb3c26
Sarbraj Dulai <sarbrajdulai01@gmail.com>
2024-07-10 09:58:15 +1000

    Adding config file without the config data

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -4,12 +4,7 @@ import { getFirestore } from 'firebase/firestore';
 
 // Web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyBB1uNFaij24uMkHKmDHUHxrECw6biNfk8',
-  authDomain: 'coding-challenge-brizzy.firebaseapp.com',
-  projectId: 'coding-challenge-brizzy',
-  storageBucket: 'coding-challenge-brizzy.appspot.com',
-  messagingSenderId: '176883651301',
-  appId: '1:176883651301:web:c54a5f56e031d7295d7a0c',
+
 };
 
 const app = initializeApp(firebaseConfig);
